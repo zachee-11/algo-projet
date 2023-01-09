05b993193c100aa521f619c85d888e4fec0e3d95
zachee-11 <121854731+zachee-11@users.noreply.github.com>
2023-01-09 19:36:35 +0100

    Update MOTEUR_TOOLS.c

--- MOTEUR_TOOLS.c
+++ MOTEUR_TOOLS.c
@@ -1,18 +1,85 @@
 #include "MOTEUR_TOOLS.h"
-
 #define DEBUG_MODE
-
 #include "DEBUG_TOOLS.h"
-
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <string.h>
-
 #include <ctype.h>
 
+//....................ouvrir un dossier....................................
 
+void base_data(char *folder)
+
+ {
+
+	DIR *d;
+
+	struct dirent *dir;
+
+	d = opendir(folder);
+
+	if (d) {
+
+	while ((dir = readdir(d)) != NULL) {
+
+
+
+	if (strcmp(dir->d_name, ".") == 0 || strcmp(dir->d_name, "..") == 0)    // J'ignore les fichiers "." et ".."
+
+		    continue;
+
+	if(dir->d_type==DT_DIR)                                              // on ignore les dossiers : 
+
+		    continue;                                     
+
+	printf("Nom du fichier: %s\n", dir->d_name);
+
+	 lire_fichier(dir->d_name); 
+
+	}
+
+	closedir(d);
+
+	
+
+	}
+
+	return;
+
+ }
+
+//---------------------------------lier un fichier----------------------------------------
+
+void lire_fichier(char *nom_fichier) 
+
+ {
+	                                              // On ouvre le fichier en lecture
+	FILE *fichier = fopen(nom_fichier, "r");
+	                                               // On vérifie que le fichier a bien été ouvert
+	if (fichier == NULL) {
+
+	    printf("Erreur d'ouverture du fichier %s\n", nom_fichier);
+
+	    exit(EXIT_FAILURE);
+
+	}                                               // On lit le contenu du fichier ligne par ligne
+
+	char ligne[256];
+
+	while (fgets(ligne, sizeof(ligne), fichier) != NULL) {
+
+	    printf("%s", ligne);
+
+	}
+	                                                   // On ferme le fichier
+
+	fclose(fichier);
+    
+	return ;
+
+ }
+
+//.......................compter occurrence mot........................................
 
 int occurrences_str(const char *str, const char *word) {
 
@@ -37,18 +104,13 @@ int occurrences_str(const char *str, const char *word) {
     return count;
 
 }
-
-
-
-
+//--------------------------------compter nombre de mot du fichier------------------------------------------
 
 int occurrences_file(FILE* file, const char *word) {
 
     int count = 0;
 
     char buffer[1024];
-
-
 
     while (fgets(buffer, sizeof(buffer), file) != NULL) {
 
@@ -60,22 +122,15 @@ int occurrences_file(FILE* file, const char *word) {
 
 }
 
+//-------------------rendre uniforme la casse---------------------------------
 
-
-
-
-
-
-int convertisseur(int argc, char *argv[]) {
+/*int convertisseur(int argc, char *argv[]) {
 
     FILE *file;
 
     FILE *clean_file;
 
     int c;
-
-    
-
 // Vérifiez qu'un argument a été passé au programme afin d'éviter une erreur de segmentation
 
     if (argc != 2) {
@@ -86,16 +141,12 @@ int convertisseur(int argc, char *argv[]) {
 
     }
 
-    
-
 // Ouverture des fichier normal et clean
 
-    clean_file = fopen ("clean_file.txt","a");
+    clean_file = fopen (clean_file,a");
 
     file = fopen(argv[1], "r");
-
     
-
 // Verifier si le fichier n'est pas NULL sinon affiche une erreur
 
     if (file == NULL) {
@@ -103,31 +154,19 @@ int convertisseur(int argc, char *argv[]) {
         perror(argv[1]);
 
         return 1;
-
     }
-
-    
-
 // Convertir le fichier en minuscule ainsi que trier les caracteres sans signe de ponctuations
-
     while ((c = fgetc(file)) != EOF) {
 
     if (!ispunct(c)) {
 
     	    fputc(c,clean_file);
 
-            putchar(tolower(c));
-
-        }
-
+           putchar(tolower(c));
+       }
     }
-
-   
-
     fclose(file);
-
     fclose(clean_file);
-
     return 0;
-
-}
+}  *****************************CE PROGRAMME EST à REVOIR CAR IL EST TROP LONG***********************************************************
+*/
