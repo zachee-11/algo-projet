ab763fdb2b7020d45725a4ad8e6862ec267d3e29
zachee-11 <121854731+zachee-11@users.noreply.github.com>
2023-01-09 19:19:22 +0100

    Update MOTEUR_TOOLS.h

--- MOTEUR_TOOLS.h
+++ MOTEUR_TOOLS.h
@@ -1,20 +1,27 @@
 #ifndef MOTEUR_TOOLS_H
-
+#include <stdlib.h>
 #define MOTEUR_TOOLS_H
-
+#include <dirent.h>
 #include <stdio.h>
 
+//NOUS CLASSERONS NOS FONCTIONS DEFINIES DANS L'ORDRE D'EXECUTION DU PROGRAMME 
+
+  void base_data(char *folder);              
+
+  void lire_fichier(char *nom_fichier) ;
+
+  int convertisseur(int argc, char *argv[]);
+
+  int occurrences_str(const char *str, const char *word);
+
+  int occurrences_file(FILE* file, const char *word);
+
+  void ecrire_fichier_binaire(char *mot, int nb_occ);
+  
+  //à compléter...
 
 
-int occurrences_str(const char *str, const char *word);
 
-
-
-int occurrences_file(FILE* file, const char *word);
-
-
-
-int convertisseur(int argc, char *argv[]);
 
 
 
